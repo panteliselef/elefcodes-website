a92360e078b88024714260f1a22d7030b1adf687
elefcodes <panteliselef@outlook.com>
2019-02-04 20:55:59 +0200

    Version before adding Redux

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -2,16 +2,20 @@ import React from "react";
 import loading from  "../images/loading.svg"
 import { NavLink } from 'react-router-dom';
 import linkIcon from '../images/link-variant.svg';
+import githubIcon from '../images/github-circle.svg';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-const ProjectCard  = ({projectName,descrition,url}) => {
+const ProjectCard  = ({projectName,description,url}) => {
 
   return (
     <div className="project-card">
       <div className="top-section">
 
-        <img src={loading} style={{transform:'translateY(-0.4em)',width:'50%'}}>
+        <div style={{backgroundImage:`url(${githubIcon})`,backgroundSize:'cover',backgroundPosition:'center center',height:'calc(90px + 0.6em)',width:'13em',opacity:'0.5'}}>
 
-        </img>
+        </div>
+        {/* <img src={githubIcon} style={{transform:'translateY(-0.4em)',width:'35%',paddingLeft:'10%'}}>
+
+        </img> */}
         <div className="top-section-image">
 
         </div>
@@ -30,7 +34,7 @@ const ProjectCard  = ({projectName,descrition,url}) => {
           </Row>
           <Row>
             <div className="details">
-              {descrition?descrition:"Description of the project. Description of the project. Description of the project"}
+              {description?description:"Description of the project. Description of the project. Description of the project"}
             </div>
 
           </Row>
