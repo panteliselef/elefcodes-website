9374768846fd9ab8bb5ea3f4583141963970da27
PANTELEIMONAS ELEFTHERIADIS <csd3942@csd.uoc.gr>
2019-11-08 12:46:21 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.49549cd56686ed516d65a6e0f4e2a602.js"
+  "/precache-manifest.3df4845482ba43dd513abf098409bbce.js"
 );
 
 workbox.clientsClaim();
